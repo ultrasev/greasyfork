2606a0b86d974578262bea655fec65ddefaae87e
ultrasev <i@ultrasev.com>
2026-08-17 11:06:40 +0800

    feat(gying_redirect): 匹配 gying.com 全站域名

--- public/gying_redirect.user.js
+++ public/gying_redirect.user.js
@@ -2,7 +2,7 @@
 // @name         gying.net 跳转
 // @name:zh-CN   gying.net 跳转
 // @namespace    https://example.com/
-// @version      1.0
+// @version      1.1
 // @description  从 gying.net 自动跳转到主站
 // @description:zh-CN 打开 gying.net 时，自动跳转到主站
 // @license      MIT
@@ -10,6 +10,8 @@
 // @downloadURL  https://greasyfork.tpz.works.dev/gying_redirect.user.js
 // @match        https://gying.net/*
 // @match        https://www.gying.net/*
+// @match        https://gying.com/*
+// @match        https://www.gying.com/*
 // @run-at       document-start
 // @grant        none
 // ==/UserScript==
